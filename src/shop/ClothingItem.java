871556fe584e063a24441ae4e6bed34bf3ee7539
mella <mella@Diego>
2026-03-16 21:04:06 +0100

    Parte 1

--- src/shop/ClothingItem.java
+++ src/shop/ClothingItem.java
@@ -1,5 +1,43 @@
 package shop;
 
 public class ClothingItem {
-
+	
+	private String name; 
+	private double price; 
+	private char size; 
+	
+	public ClothingItem(String name, double price, char size) {
+		
+		this.name = name; 
+		this.price = price; 
+		if(size == 'S' || size == 'M' || size == 'L') {
+			this.size = size; 
+		}else {
+			this.size = 'M'; 
+		}
+		
+	}
+	
+	public String getName() {return this.name;}
+	public double getPrice() {return this.price;}
+	public char getSize() {return this.size;}
+	
+	public void setName(String name) {this.name = name;}
+	public void setPrice(double price) {this.price = price;}
+	public void setSize(char size) {
+		if(size == 'S' || size == 'M' || size == 'L') {
+			this.size = size;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return this.name + " - " + this.price + "€ Talla: " + this.size;
+	}
+	
+	
 }
+
+
+
+
